37592b755fa25b73c8ebf495fa88f9114f48b5c3
alex <aabinaei@gmail.com>
2025-01-20 06:27:42 -0800

    windows packager

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(non_snake_case)]
+
 mod error;
 mod gateway;
 mod signer;
